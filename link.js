22d6df36a9a1493de5cfed8d744352be85e28482
llsonkimm <lsoncheruiyot@gmail.com>
2025-03-14 22:34:57 +0300

    Fith commit

--- link.js
+++ link.js
@@ -240,3 +240,57 @@ function range(start, end, step = 1) {
   let arrayValue = [1, 2, 3, 4, 5];
   reverseArrayInPlace(arrayValue);
   console.log(arrayValue); // → [5, 4, 3, 2, 1]
+
+  // pass action as a function value
+// function repeat(n, action){
+//     for(let i = 0; i < n; i++){
+//         action(i);
+//     }
+// }
+// repeat(3, console.log);
+// // creating a function value on the spot
+// let labels = [];
+// repeat (5, i=>{
+//     labels.push('Unit ${i + 1}');
+// });
+// console.log(labels);
+
+// we can have functions that create new functions
+function greaterThan(n){
+  return m => m > n;
+}
+let greaterThan10 = greaterThan(10);
+console.log(greaterThan10(11));
+
+
+
+// functions that change other functions
+function noisy(f){
+  return(...args) => {
+      console.log("Calling with ", args);
+      let result = f(...args);
+      console.log("called with ", args, ", returned", result);
+      return result;
+  };
+}
+noisy(Math.min)(3,2,1);
+
+// functions that provide new types of control flow
+function unless(test, then){
+  if(!test) then();
+}
+function repeat(times, action) {
+  for (let i = 0; i < times; i++) {
+      action(i);
+  }
+}
+
+repeat(3, n => {
+  unless(n % 2 == 1, () => {
+      console.log(n, "is Even");
+  });
+});
+
+
+// built-in array method, forEach, that provides something like a for/of loop
+["A", "B"].forEach(l => console.log(l));
